fed6c5133e3162c50212d3ef3d7506aa7e206778
boeserDunklerMann <andre.dunkel@o2mail.de>
2020-11-07 12:35:18 +0100

    Testingapp and Model added

---
+++ DA.WinForms.Framework/Examples/Bookstore.Model/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bookstore.Model
+{
+	public class Book
+	{
+		public Guid Id { get; set; }
+		public string BookName { get; set; }
+		public string Author { get; set; }
+		public decimal Price { get; set; }
+	}
+}
